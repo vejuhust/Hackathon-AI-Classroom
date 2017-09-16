94cce20b54bcdbefc3d21b4775c46a83c82e1ad2
Wei Ye <vejuhust@gmail.com>
2017-09-16 06:01:40 +0000

    Import SignalR with default example

---
+++ src/TeacherPanel/Hubs/ReportHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace TeacherPanel.Hubs
+{
+    public class ReportHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Send(string name, string message)
+        {
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
